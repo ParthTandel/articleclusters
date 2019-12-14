d5b7ecaee178e66a914c87ddd886986ae83d4cd1
priyasingh16 <priyu16702@gmail.com>
2019-12-14 16:55:33 -0500

    tokenization updated

--- filterdata.py
+++ filterdata.py
@@ -21,37 +21,40 @@ except Exception:
 data  = collection.find()
 
 for article in data:
-    print("********************************************************************")
-    a = Article(article["link"])
-    a.download()
-    a.parse()
-    text = a.text
-    text_array = text.split("\n")
-    new_arr = []
+    try:
+        print("********************************************************************")
+        a = Article(article["link"])
+        a.download()
+        a.parse()
+        text = a.text
+        text_array = text.split("\n")
+        new_arr = []
 
-    for val in text_array:
-        if val.count("Photos:") > 0 or val.count("Chat with us in Facebook Messenger") > 0 or val.count("Read or Share this story:") > 0 or \
-           val.count("entertainment.news@bbc.co.uk") > 0 or val.count("newstips.usatoday.com") > 0:
-            continue
-        new_arr.append(val)
+        for val in text_array:
+            if val.count("Photos:") > 0 or val.count("Chat with us in Facebook Messenger") > 0 or val.count("Read or Share this story:") > 0 or \
+            val.count("entertainment.news@bbc.co.uk") > 0 or val.count("newstips.usatoday.com") > 0:
+                continue
+            new_arr.append(val)
 
-    text = " ".join(new_arr)
-    text = re.sub("r'(https|http)*[:.]+\S+'", " ", text)
-    text = re.sub("'s", " ", text)
-    text = re.sub("CLOSE", " ", text)
-    text = re.sub("USA TODAY", " ", text)
-    text = re.sub("Post to Facebook", " ", text)
-    text = re.sub("Image copyright Getty Images Image caption", " ", text)
-    text = re.sub("Image copyright PA Image caption", " ", text)
-    text = re.sub("Media playback is unsupported on your device Media caption This video has been removed for rights reasons", " ", text)
-    text = re.sub("\(CNN\)", " ", text)
-    text = re.sub("Story highlights", " ", text)
-    article_text = re.sub("Read More", " ", text)
+        text = " ".join(new_arr)
+        text = re.sub("r'(https|http)*[:.]+\S+'", " ", text)
+        text = re.sub("'s", " ", text)
+        text = re.sub("CLOSE", " ", text)
+        text = re.sub("USA TODAY", " ", text)
+        text = re.sub("Post to Facebook", " ", text)
+        text = re.sub("Image copyright Getty Images Image caption", " ", text)
+        text = re.sub("Image copyright PA Image caption", " ", text)
+        text = re.sub("Media playback is unsupported on your device Media caption This video has been removed for rights reasons", " ", text)
+        text = re.sub("\(CNN\)", " ", text)
+        text = re.sub("Story highlights", " ", text)
+        article_text = re.sub("Read More", " ", text)
 
-    article_tokens = [word for word in article_text.lower().split() if str(word.encode('utf-8')) not in stopwords]
-    article["refinedtext"] = " ".join(article_tokens)
-    article["count"] = count
-    if len( article_tokens ) > 150:
-        print (final_collection.insert_one(article).inserted_id,article["link"])
-        count = count + 1
-    print ("********************************************************************")
+        article_tokens = [word for word in article_text.lower().split() if str(word.encode('utf-8')) not in stopwords]
+        article["refinedtext"] = " ".join(article_tokens)
+        article["count"] = count
+        if len( article_tokens ) > 150:
+            print (final_collection.insert_one(article).inserted_id,article["link"], count)
+            count = count + 1
+        print ("********************************************************************")
+    except Exception as e:
+        print("Error: ", str(e))
--- tokenization.py
+++ tokenization.py
@@ -1,128 +1,49 @@
-import nltk
 import spacy
 import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
-
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
+from stopword_rm import stopwords
 from gensim import corpora, models
 from collections import defaultdict
 from pymongo import MongoClient
-client = MongoClient()
-db = client.projectdb1
+
+# connecting to ec2 mongodb
+client = MongoClient("ec2-18-223-123-230.us-east-2.compute.amazonaws.com")
+db = client.projectclean_db
 collection = db.articles
-nlp = spacy.load('en')
-from stopword_rm import stopwords
+nlp = spacy.load('en_core_web_sm')
 
 documents = []
 count = 1
 
-
 frequency = defaultdict(int)
 
-data = collection.find({"type":"Entertainment"})
-data_len = collection.find({"type":"Entertainment"}).count()
+data = collection.find()
+data_len = collection.find().count()
 for article in data:
     article_text = article["text"].split(".")[3:]
+    article_text = " ".join(article_text)
+    article_text = article_text.split(" ")
     article_text = [str(word) for word in article_text if str(word) not in stopwords]
-    article_text = "".join(article_text)
-    # texts = [str(word) for word in article_text.lower().split() if str(word) not in stopwords]
-    # documents.append(texts)
-
-    # print article["count"]," ",article["link"]
-
-    # print " ".join(texts)
-    # tokens = nltk.word_tokenize(article["text"])
-
-    doc = nlp(unicode(article_text))
-    # tagged = nltk.pos_tag(tokens)
-    # entities = nltk.chunk.ne_chunk(tagged)
-    # print tagged
+    article_text = " ".join(article_text)
+    
+    doc = nlp(article_text)
+    
     spacy_tokens = []
     for ent in doc.ents:
         if ent.label_ == "PERSON" or ent.label_ == "GPE" or ent.label_ == "ORG":
-            print (ent.text, ent.label_)
+            print(ent.text.lower())
             if str(ent.text.lower()) not in stopwords:
                 frequency[ent.text.lower()] = frequency[ent.text.lower()] + (article_text).lower().count(ent.text.lower())
                 spacy_tokens.append(ent.text.lower())
-    #
+    
     documents.append(spacy_tokens)
-    # print count
-    # count = count + 1
-    # print "***************************************************************************"
-    # texts = [word for word in article_text.lower().split() if str(word) not in stopwords]
+   
+texts = [[str(token).lower() for token in text if frequency[str(token).lower()] > 1] for text in documents]
 
-    # for word in spacy_tokens:
-    #     print word
-
-    # print "***************************************************************************"
-
-    # documents.append(texts)
-    # print texts
-    # count = count + 1
-    # if count == 10:
-    #     break
-
-
-# frequency = defaultdict(int)
-#
-# for text in documents:
-#     for token in text:
-#         frequency[str(token).lower()] += 1
-#
-texts = [[str(token).lower() for token in text if frequency[str(token).lower()] > 1]for text in documents]
-
-#
-# will_count = 0
-# trump_count = 0
-# facebook_count = 0
-# news_count = 0
-#
-#
-# for data in texts:
-#     print "***************************************************************************"
-#
-#     trump_true = False
-#     facebook_true = False
-#     will_true = False
-#     news_true = False
-#
-#
-#     for word in data:
-#         print word
-#
-#         if word.lower() == "trump" and trump_true == False:
-#             trump_count = trump_count + 1
-#             trump_true = True
-#
-#         if word.lower() == "will" and will_true == False:
-#             will_count = will_count + 1
-#             will_true = True
-#
-#
-#         if word.lower() == "facebook" and facebook_true == False:
-#             facebook_count = facebook_count + 1
-#             facebook_true = True
-#
-#         if word.lower() == "news" and news_true == False:
-#             news_count = news_count + 1
-#             news_true = True
-#
-#     print "***************************************************************************"
-# #
-# print trump_count
-# print will_count
-# print facebook_count
-# print news_count
-
-# texts = documents
-# #
-# print texts
-# #
 dictionary = corpora.Dictionary(texts)
 dictionary.save('deerwester.dict')
-# print(dictionary.token2id)
+
 corpus = [dictionary.doc2bow(text) for text in texts]
+print(corpus[0])
 corpora.MmCorpus.serialize('deerwester.mm', corpus)
-#
-# print frequency
-# print dictionary
