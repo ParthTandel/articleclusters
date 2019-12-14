328c6688a394e3d0b83acf19cd9d6b70b1f31419
priyasingh16 <priyu16702@gmail.com>
2019-12-14 17:20:32 -0500

    lda model updated

--- model_topics.py
+++ model_topics.py
@@ -1,67 +1,56 @@
 import os
-import nltk
 import spacy
 from gensim import corpora,models, similarities
-
-from pymongo import MongoClient
-client = MongoClient()
-db = client.projectdb1
-collection = db.articles
-nlp = spacy.load('en')
 from stopword_rm import stopwords
+from pymongo import MongoClient
 
-
+client = MongoClient("ec2-18-223-123-230.us-east-2.compute.amazonaws.com")
+db = client.projectclean_db
+collection = db.articles
+nlp = spacy.load('en_core_web_sm')
 
 if (os.path.exists("deerwester.dict")):
     dictionary = corpora.Dictionary.load('deerwester.dict')
     corpus = corpora.MmCorpus('deerwester.mm')
-    model = models.ldamodel.LdaModel(corpus, id2word=dictionary, num_topics=5)
-    topics = model.print_topics(5)
+    model = models.ldamodel.LdaModel(corpus, id2word=dictionary, num_topics=100)
+    topics = model.print_topics(100)
 
     for topic in topics:
-        print topic
+        print(topic)
 
     clusters = {}
-    for i in xrange(0,10):
+    for i in range(0,100):
         clusters[i] = []
 
     data = collection.find({"type":"Entertainment"})
     for article in data:
-        # article_text = article["text"].split(".")[3:]
-        # article_text = [str(word) for word in article_text if str(word) not in stopwords]
-        # article_text = u"".join(article_text).encode('utf-8')
-
+        article_text = article["text"].split(".")[3:]
+        article_text = " ".join(article_text)
+        article_text = article_text.split(" ")
+        article_text = [str(word) for word in article_text if str(word) not in stopwords]
+        article_text = " ".join(article_text)
 
         spacy_tokens = []
-        # doc = nlp(unicode(article_text, errors='ignore'))
         doc = nlp(article["text"])
         for ent in doc.ents:
             if ent.label_ == "PERSON" or ent.label_ == "GPE" or ent.label_ == "ORG" and ent.text.lower() not in stopwords:
                 spacy_tokens.append(ent.text.lower())
 
         query = dictionary.doc2bow(spacy_tokens)
-        print article['count'],model[query]
+        # print article['count'],model[query]        
         similarity = model[query]
         for data in similarity:
             if data[1] > 0.9:
                 clusters[data[0]].append({
-                    "id" : article["count"],
-                    "doc": spacy_tokens
-                })
+                    "id" : article["count"]
+                    })
 
 
     # print clusters
     for value in clusters.keys():
-        print "**********************************************************************"
-        print topics[value]
-        print "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
+        print ("**********************************************************************")
+        print (topics[value])
+        print ("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
         for data in clusters[value]:
-            print data
-        print "**********************************************************************"
-
-
-   # index = similarities.MatrixSimilarity(model[corpus])
-   # index.save('deerwester.index')
-   # index = similarities.MatrixSimilarity.load('deerwester.index')
-else:
-   print("Please run first tutorial to generate data set")
+            print (data)
+        print ("**********************************************************************")
